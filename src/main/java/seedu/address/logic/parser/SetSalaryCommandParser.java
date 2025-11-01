68c7847f65ecd0945fd7aa915099039355e9df2e
Puyu <49837577+Z-Puyu@users.noreply.github.com>
2025-11-01 12:04:50 +0800

    Handle number format exceptions

--- src/main/java/seedu/address/logic/parser/SetSalaryCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SetSalaryCommandParser.java
@@ -29,16 +29,20 @@ public class SetSalaryCommandParser implements Parser<SetSalaryCommand> {
             throw new ParseException(Messages.MESSAGE_INVALID_PERSON_ID);
         }
 
-        double value = Double.parseDouble(tokens[1]);
-        if (!Double.isFinite(value)) {
-            throw new ParseException(Messages.MESSAGE_SALARY_TOO_HIGH);
-        }
+        try {
+            double value = Double.parseDouble(tokens[1]);
+            if (!Double.isFinite(value)) {
+                throw new ParseException(Messages.MESSAGE_SALARY_TOO_HIGH);
+            }
 
-        if (value < 0) {
+            if (value < 0) {
+                throw new ParseException(Messages.MESSAGE_INVALID_SALARY);
+            }
+
+            return new SetSalaryCommand(id, value);
+        } catch (NumberFormatException e) {
             throw new ParseException(Messages.MESSAGE_INVALID_SALARY);
         }
-
-        return new SetSalaryCommand(id, value);
     }
 
     /**
